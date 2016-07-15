0d803f58a3277e7857935c2884e0ae92fb4674be
Miruna Badescu <miruna@eaudeweb.ro>
2016-07-15 10:31:43 +0300

    added require.js / Header almost finished for all devices / page layout working / small changes to homepage layout

--- bise/diazotheme/static/js/navbar.js
+++ bise/diazotheme/static/js/navbar.js
@@ -5,186 +5,7 @@
 
 
 
-// DE AICI MODIFIC
-
-
-/* ========================================================================
- * Bootstrap: dropdown.js v3.3.6
- * http://getbootstrap.com/javascript/#dropdowns
- * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
-+function ($) {
-  'use strict';
-
-  // DROPDOWN CLASS DEFINITION
-  // =========================
-
-  var backdrop = '.dropdown-backdrop'
-  var toggle   = '[data-toggle="dropdown"]'
-  var Dropdown = function (element) {
-    $(element).on('click.bs.dropdown', this.toggle)
-  }
-
-  Dropdown.VERSION = '3.3.6'
-
-  function getParent($this) {
-    var selector = $this.attr('data-target')
-
-    if (!selector) {
-      selector = $this.attr('href')
-      selector = selector && /#[A-Za-z]/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
-    }
-
-    var $parent = selector && $(selector)
-
-    return $parent && $parent.length ? $parent : $this.parent()
-  }
-
-  function clearMenus(e) {
-    if (e && e.which === 3) return
-    $(backdrop).remove()
-    $(toggle).each(function () {
-      var $this         = $(this)
-      var $parent       = getParent($this)
-      var relatedTarget = { relatedTarget: this }
-
-      if (!$parent.hasClass('open')) return
-
-      if (e && e.type == 'click' && /input|textarea/i.test(e.target.tagName) && $.contains($parent[0], e.target)) return
-
-      $parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
-
-      if (e.isDefaultPrevented()) return
-
-      $this.attr('aria-expanded', 'false')
-      $parent.removeClass('open').trigger($.Event('hidden.bs.dropdown', relatedTarget))
-    })
-  }
-
-  Dropdown.prototype.toggle = function (e) {
-    var $this = $(this)
-
-    if ($this.is('.disabled, :disabled')) return
-
-    var $parent  = getParent($this)
-    var isActive = $parent.hasClass('open')
-
-    clearMenus()
-
-    if (!isActive) {
-      if ('ontouchstart' in document.documentElement && !$parent.closest('.navbar-nav').length) {
-        // if mobile we use a backdrop because click events don't delegate
-        $(document.createElement('div'))
-          .addClass('dropdown-backdrop')
-          .insertAfter($(this))
-          .on('click', clearMenus)
-      }
-
-      var relatedTarget = { relatedTarget: this }
-      $parent.trigger(e = $.Event('show.bs.dropdown', relatedTarget))
-
-      if (e.isDefaultPrevented()) return
-
-      $this
-        .trigger('focus')
-        .attr('aria-expanded', 'true')
-
-      $parent
-        .toggleClass('open')
-        .trigger($.Event('shown.bs.dropdown', relatedTarget))
-    }
-
-    return false
-  }
-
-  Dropdown.prototype.keydown = function (e) {
-    if (!/(38|40|27|32)/.test(e.which) || /input|textarea/i.test(e.target.tagName)) return
-
-    var $this = $(this)
-
-    e.preventDefault()
-    e.stopPropagation()
-
-    if ($this.is('.disabled, :disabled')) return
-
-    var $parent  = getParent($this)
-    var isActive = $parent.hasClass('open')
-
-    if (!isActive && e.which != 27 || isActive && e.which == 27) {
-      if (e.which == 27) $parent.find(toggle).trigger('focus')
-      return $this.trigger('click')
-    }
-
-    var desc = ' li:not(.disabled):visible a'
-    var $items = $parent.find('.dropdown-menu' + desc)
-
-    if (!$items.length) return
-
-    var index = $items.index(e.target)
-
-    if (e.which == 38 && index > 0)                 index--         // up
-    if (e.which == 40 && index < $items.length - 1) index++         // down
-    if (!~index)                                    index = 0
-
-    $items.eq(index).trigger('focus')
-  }
-
-
-  // DROPDOWN PLUGIN DEFINITION
-  // ==========================
-
-  function Plugin(option) {
-    return this.each(function () {
-      var $this = $(this)
-      var data  = $this.data('bs.dropdown')
-
-      if (!data) $this.data('bs.dropdown', (data = new Dropdown(this)))
-      if (typeof option == 'string') data[option].call($this)
-    })
-  }
-
-  var old = $.fn.dropdown
-
-  $.fn.dropdown             = Plugin
-  $.fn.dropdown.Constructor = Dropdown
-
-
-  // DROPDOWN NO CONFLICT
-  // ====================
-
-  $.fn.dropdown.noConflict = function () {
-    $.fn.dropdown = old
-    return this
-  }
-
-
-  // APPLY TO STANDARD DROPDOWN ELEMENTS
-  // ===================================
-
-  $(document)
-    .on('click.bs.dropdown.data-api', clearMenus)
-    .on('click.bs.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
-    .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
-    .on('keydown.bs.dropdown.data-api', toggle, Dropdown.prototype.keydown)
-    .on('keydown.bs.dropdown.data-api', '.dropdown-menu', Dropdown.prototype.keydown)
-
-}(jQuery);
-
-
-
-$( document ).ready(function() {
-    console.log( "ready!" );
-
-
-
-
-
-
-(function(){
+$( document ).ready(function(){
 
   // data-toggle="dropdown"
 
@@ -192,20 +13,53 @@ $( document ).ready(function() {
   
   // data-toggle='navmenu';
   
+
+
+  // alert('merge');
+document.getElementById('navsearch-input').addEventListener('focus', function() {
+  console.log(this.parentElement);
+  this.parentElement.parentElement.classList.add('navsearch-open');
+}, false);
+
+document.getElementById('navsearch-input').addEventListener('blur', function() {
+  this.parentElement.parentElement.classList.remove('navsearch-open');
+}, false);
+
+
+
+
+
 console.log('merge');
 
-  $triggers=$("[data-toggle='navmenu']");
+ $triggers = $('[data-toggle="navmenu"]');
+
+
+$triggers.each(function() {
+  var $trigger = $(this),
+      $target = $(this).closest('.navmenu-item')
+                       .find('.navgroups-body')
+                        .first();
+    $trigger.click(function() {
+      $target.toggleClass('open');
+     
+    // $target.delay( 100 ).scrollTo(150, 200);
+
+    });
+  });
+
+
+
+
+
+ $triggers=$("[data-target='#navmenu-body']");
   $triggers.each(function() {
     var $trigger = $(this),
         target = $trigger.data('target'),
         $target = $(target);
-    $trigger.click(function() {
+$trigger.click(function() {
       $target.toggleClass('open');
-     
-    $target.delay( 100 ).scrollTo(150, 200);
 
     });
-  });
 
 
 
@@ -216,21 +70,33 @@ console.log('merge');
 $navmenuopen=$("#navmenu-open");
 $navmenuclose=$("#navmenu-close");
 $navmenubody=$(".navmenu-body");
-
+$asidetrigger=$(".asidetrigger")
+$aside=$("aside")
 
 $navmenuopen.click(function(){
   // alert('merge');
-$navmenuopen.addClass('close');
-$navmenuclose.removeClass('close');
+$navmenuopen.addClass('hidden');
+$navmenuclose.removeClass('hidden');
 // $navmenubody.addClass('tablet');
 });
 
 
 
+$asidetrigger.click(function(){
+$aside.addClass("triggered")
+$body.addClass('sidebaropen');
+});
+
+$backdrop.click(function(){
+$aside.removeClass("triggered")
+$body.removeClass('sidebaropen');
+});
+
+
 $navmenuclose.click(function(){
   // alert('merge2');
-$navmenuclose.addClass('close');
-$navmenuopen.removeClass('close');
+$navmenuclose.addClass('hidden');
+$navmenuopen.removeClass('hidden');
 // $navmenubody.removeClass('tablet');
 });
 
@@ -238,72 +104,35 @@ $navmenuopen.removeClass('close');
 
 
 
-
-
-
-
-  $navmenuclose.click(function () {
-          // $navmenuclose.addClass('close');
-// $navmenuopen.removeClass('close');
-$body.addClass('sidebaropen');
-$navmenubody.addClass('tablet');
-// $links.addClass('quicklinks');
-        });
-
-
-         $navmenuopen.click(function () {
-          // $navmenuclose.addClass('close');
-// $navmenuopen.removeClass('close');
-$body.addClass('sidebaropen');
-$navmenubody.addClass('tablet');
-// $links.addClass('quicklinks');
-
-        });
-
         $backdrop.on('click', function () {
             console.log('closing sidebar');
             // $filters.removeClass('open');
-           $navmenubody.removeClass('tablet');
+           // $navmenubody.removeClass('tablet');
            $body.removeClass('sidebaropen');
-           $navmenuclose.addClass('close');
-$navmenuopen.removeClass('close');
+           $navmenuclose.addClass('hidden');
+$navmenuopen.removeClass('hidden');
 // $links.removeClass('quicklinks');
         });
   
-  }());
-
-
-
-
-  // alert('merge');
-document.getElementById('input-search').addEventListener('focus', function() {
-  console.log(this.parentElement);
-  this.parentElement.parentElement.classList.add('search-open');
-}, false);
-
-document.getElementById('input-search').addEventListener('blur', function() {
-  this.parentElement.parentElement.classList.remove('search-open');
-}, false);
-
-
   });
 
 
 
 
 
+function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,''])[1]
+    );
+}
+var q = getURLParameter("q");
+q = q.replace("+", " ");
+if (q != ''){$("#catalogue-app").attr("data-query", q);}
 
 
 
-  //scrollto
+  }(jQuery));
 
-  /**
- * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
- * Licensed under MIT
- * @author Ariel Flesler
- * @version 2.1.0
- */
-;(function(l){'use strict';l(['jquery'],function($){var k=$.scrollTo=function(a,b,c){return $(window).scrollTo(a,b,c)};k.defaults={axis:'xy',duration:0,limit:true};function isWin(a){return!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!==-1}$.fn.scrollTo=function(f,g,h){if(typeof g==='object'){h=g;g=0}if(typeof h==='function'){h={onAfter:h}}if(f==='max'){f=9e9}h=$.extend({},k.defaults,h);g=g||h.duration;var j=h.queue&&h.axis.length>1;if(j){g/=2}h.offset=both(h.offset);h.over=both(h.over);return this.each(function(){if(f===null)return;var d=isWin(this),elem=d?this.contentWindow||window:this,$elem=$(elem),targ=f,attr={},toff;switch(typeof targ){case'number':case'string':if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=d?$(targ):$(targ,elem);if(!targ.length)return;case'object':if(targ.is||targ.style){toff=(targ=$(targ)).offset()}}var e=$.isFunction(h.offset)&&h.offset(elem,targ)||h.offset;$.each(h.axis.split(''),function(i,a){var b=a==='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,prev=$elem[key](),max=k.max(elem,a);if(toff){attr[key]=toff[pos]+(d?0:prev-$elem.offset()[pos]);if(h.margin){attr[key]-=parseInt(targ.css('margin'+b),10)||0;attr[key]-=parseInt(targ.css('border'+b+'Width'),10)||0}attr[key]+=e[pos]||0;if(h.over[pos]){attr[key]+=targ[a==='x'?'width':'height']()*h.over[pos]}}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)==='%'?parseFloat(c)/100*max:c}if(h.limit&&/^\d+$/.test(attr[key])){attr[key]=attr[key]<=0?0:Math.min(attr[key],max)}if(!i&&h.axis.length>1){if(prev===attr[key]){attr={}}else if(j){animate(h.onAfterFirst);attr={}}}});animate(h.onAfter);function animate(a){var b=$.extend({},h,{queue:true,duration:g,complete:a&&function(){a.call(elem,targ,h)}});$elem.animate(attr,b)}})};k.max=function(a,b){var c=b==='x'?'Width':'Height',scroll='scroll'+c;if(!isWin(a))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,doc=a.ownerDocument||a.document,html=doc.documentElement,body=doc.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return $.isFunction(a)||$.isPlainObject(a)?a:{top:a,left:a}}$.Tween.propHooks.scrollLeft=$.Tween.propHooks.scrollTop={get:function(t){return $(t.elem)[t.prop]()},set:function(t){var a=this.get(t);if(t.options.interrupt&&t._last&&t._last!==a){return $(t.elem).stop()}var b=Math.round(t.now);if(a!==b){$(t.elem)[t.prop](b);t._last=this.get(t)}}};return k})}(typeof define==='function'&&define.amd?define:function(a,b){'use strict';if(typeof module!=='undefined'&&module.exports){module.exports=b(require('jquery'))}else{b(jQuery)}}));
 
 
 
